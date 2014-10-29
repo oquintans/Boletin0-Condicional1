ba07da4499c0b113b2355a19de4169fa4efa9629
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-10-29 11:11:09 +0100

    finish

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -6,12 +6,9 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
         Condicional par1 = new Condicional();
-        Condicional par2 = new Condicional();
-        Condicional resultado = new Condicional();
         double num1=par1.pedirNum();
-        double num2=par2.pedirNum();
-        resultado.getCondicion(num1, num2);
-        
+        double num2=par1.pedirNum();
+        par1.getCondicion(num1, num2);        
     }
 
 }
